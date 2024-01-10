0d7079e4a345782da3056fa033e2757f20196de0
Gyurkó Levente Péter <gleventepter@mitiszkad.lan>
2024-01-10 12:09:41 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  NavLink,
+  Routes,
+  Route,
+} from "react-router-dom";
+import "./App.css";
+import { PizzaHome } from "./PizzaHome";
+import { PizzaMod } from "./PizzaMod";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink to={`/`} className="nav-link">
+                <span className="nav-link">Home</span>
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to={`/`} className="nav-link">
+                <span className="nav-link">Új hangszer</span>
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/" element={<InstrumentListPage />} />
+        <Route
+          path="/hangszer/:hangszerId"
+          element={<InstrumentSinglePage />}
+        />
+        <Route path="uj-hangszer" element={<InstrumentCreatePage />} />
+      </Routes>
+    </Router>
   );
 }
 
